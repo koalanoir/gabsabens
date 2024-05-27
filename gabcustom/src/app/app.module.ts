3bcacfe386fef86a34450729001e63b269307a85
NGOMA SITA <dorian.ngoma-sita@capgemini.com>
2024-05-27 05:21:26 +0200

    end v1

--- gabcustom/src/app/app.module.ts
+++ gabcustom/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule} from'@angular/material/icon';
+import {MatCardModule} from '@angular/material/card'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -8,18 +9,23 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { ProcessusComponent } from './processus/processus.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    ProcessusComponent,
     HomeComponent,
     FooterComponent,
     ProjectsComponent,
+    PresentationComponent,
     ContactComponent
   ],
   imports: [
+    MatCardModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
